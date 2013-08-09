df87219a4c1f0e56150ef212df2320da1c933de2
CubeSugar <wei.jian.coder+github@gmail.com>
2013-08-10 00:12:24 +0800

    update:计算余弦相似度伪代码

---
+++ bin/CalcSimilarity_CalcDocVector.cpp
@@ -0,0 +1,31 @@
+/*
+	map<string, vector<string> > AllWords;
+	
+	map<string, vector<string> >::iterator itrAll = AllWords.begin();
+	Documents document;
+
+	//for the document
+	DFw1 = (itrAll->second).size() / 2;
+	TFd = dcoument.FrqWords[itrAll->first];
+	W1 = TFd * DFw1;
+	.
+	.
+	.
+
+	DFwn = (itrAll->second).size() / 2;
+	TFd = document.FrqWords[itrALl->first];
+	Wn = TFd * DFwn;
+
+	SumW = W1 + ... + Wn;
+
+	StdW1 = W1 / SumW;
+	.
+	.
+	.
+	StdWn = Wn / SumW;
+	
+	Document.DocVecotr.push_back(Stdwi);
+
+*/
+
+ //int calcDocVector(const set<string, vector<string> > &allwords, const Document &document, vector<double> &DocVector);
